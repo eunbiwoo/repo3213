c04daab8f0a5753c88ebed42c59693d6e30dcc9d
eunbi <w99962871@gmail.com>
2024-03-11 16:20:42 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
+        //문제해결
+        int ret = getSum(10, 20);
+
+
+
+
+
+
+    }
+
+    private static int getSum(int aaa, int value2) {
+        return aaa = value2;
     }
 }
